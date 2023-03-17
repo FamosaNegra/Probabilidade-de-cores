b8a9243beb8674c3d8d0e6c6eeb315319548c712
FamosaNegra <114776724+FamosaNegra@users.noreply.github.com>
2023-03-17 18:18:55 -0300

    agora possui 3 botões e salva as informações no localstorage do usuario mesmo quando recarrega a pagina ele puxa o historico e continua a analise

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
+let whiteCount = parseInt(localStorage.getItem("whiteCount")) || 0;
+let blackCount = parseInt(localStorage.getItem("blackCount")) || 0;
+let redCount = parseInt(localStorage.getItem("redCount")) || 0;
 
-  let whiteCount = 0;
-  let blackCount = 0;
-  let redCount = 0;
+function calculateProb() {
+  let total = whiteCount + blackCount + redCount;
+  let whiteProb = (whiteCount / total) * 100;
+  let blackProb = (blackCount / total) * 100;
+  let redProb = (redCount / total) * 100;
 
-  function calculateProb() {
-    let lastColor = document.getElementById("last-color").value;
+  document.getElementById("qtd-branco").textContent = whiteCount;
+  document.getElementById("prob-branco").textContent = whiteProb.toFixed(2) + "%";
 
-    if (lastColor !== "none") {
-      switch (lastColor) {
-        case "branco":
-          whiteCount++;
-          break;
-        case "preto":
-          blackCount++;
-          break;
-        case "vermelho":
-          redCount++;
-          break;
-      }
-    }
+  document.getElementById("qtd-preto").textContent = blackCount;
+  document.getElementById("prob-preto").textContent = blackProb.toFixed(2) + "%";
 
-    let total = whiteCount + blackCount + redCount;
-    let whiteProb = (whiteCount / total) * 100;
-    let blackProb = (blackCount / total) * 100;
-    let redProb = (redCount / total) * 100;
+  document.getElementById("qtd-vermelho").textContent = redCount;
+  document.getElementById("prob-vermelho").textContent = redProb.toFixed(2) + "%";
+}
 
-    document.getElementById("qtd-branco").textContent = whiteCount;
-    document.getElementById("prob-branco").textContent = whiteProb.toFixed(2) + "%";
+// Chama a função calculateProb() quando a página for carregada
+calculateProb();
 
-    document.getElementById("qtd-preto").textContent = blackCount;
-    document.getElementById("prob-preto").textContent = blackProb.toFixed(2) + "%";
+// Adiciona event listeners aos botões para atualizar a contagem e a porcentagem
+document.getElementById("white-btn").addEventListener("click", function () {
+  whiteCount++;
+  localStorage.setItem("whiteCount", whiteCount);
+  calculateProb();
+});
 
-    document.getElementById("qtd-vermelho").textContent = redCount;
-    document.getElementById("prob-vermelho").textContent = redProb.toFixed(2) + "%";
-  };
+document.getElementById("black-btn").addEventListener("click", function () {
+  blackCount++;
+  localStorage.setItem("blackCount", blackCount);
+  calculateProb();
+});
+
+document.getElementById("red-btn").addEventListener("click", function () {
+  redCount++;
+  localStorage.setItem("redCount", redCount);
+  calculateProb();
+});
